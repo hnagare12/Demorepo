70c1c1606597f20dfd89098ad920309d64ebe812
hnagare12 <hnagare12@gmail.com>
2022-01-11 20:35:02 +0530

    First Commit

---
+++ SpringBootDataJPAProj7-QueryMethod-HQL-JPQL/src/main/java/com/nt/SpringBootDataJpaProj7QueryMethodHqlJpqlApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataJpaProj7QueryMethodHqlJpqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataJpaProj7QueryMethodHqlJpqlApplication.class, args);
+	}
+
+}
---
+++ SpringBootDataJPAProj7-QueryMethod-HQL-JPQL/src/test/java/com/nt/SpringBootDataJpaProj7QueryMethodHqlJpqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDataJpaProj7QueryMethodHqlJpqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
