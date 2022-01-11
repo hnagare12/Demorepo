8fd4aca03b077ebdb5b3347cb364d3e8cf61d9be
hnagare12 <hnagare12@gmail.com>
2022-01-11 19:22:33 +0530

    Fist Commit

---
+++ SpringBootJPAProj5-finderMethod-staticProjection/src/main/java/com/nt/SpringBootJpaProj5FinderMethodStaticProjectionApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaProj5FinderMethodStaticProjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaProj5FinderMethodStaticProjectionApplication.class, args);
+	}
+
+}
---
+++ SpringBootJPAProj5-finderMethod-staticProjection/src/test/java/com/nt/SpringBootJpaProj5FinderMethodStaticProjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaProj5FinderMethodStaticProjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
