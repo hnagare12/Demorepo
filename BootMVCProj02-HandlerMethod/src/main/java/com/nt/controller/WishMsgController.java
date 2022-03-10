05bc2bea518e0ee371ea190d5a5e63e72755ad51
hnagare12 <hnagare12@gmail.com>
2022-03-10 14:13:08 +0530

    bestMapping

---
+++ BootMVCProj02-HandlerMethod/src/main/java/com/nt/controller/WishMsgController.java
@@ -0,0 +1,42 @@
+package com.nt.controller;
+
+
+import java.util.Date;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.nt.service.IWishService;
+
+@Controller
+public class WishMsgController {
+	@Autowired
+	private IWishService  service;
+	
+	@RequestMapping("/home")
+	public  String showHomePage() {
+		//return LVN
+		return "welcome";
+	}
+	
+	@RequestMapping("/wish")
+	public  String   fetchWishMessage(Map<String,Object> map) {
+		System.out.println("shared Memory obj class name::"+map.getClass());
+		//use service
+		String  msg=service.generateWishMessage();
+		 //  keep  data in model attributes
+		map.put("wMsg", msg);
+		map.put("sysDate",new Date());
+		//return MAV
+		return "show_result";
+	}
+	
+
+	
+	
+	
+	
+}
---
+++ BootMVCProj02-HandlerMethod/src/main/webapp/WEB-INF/pages/welcome.jsp
@@ -0,0 +1,4 @@
+
+<br>
+<br>
+<h1    style="color:red;text-align:center"><a href="wish">Get Wish Message</a></h1>
