66aa157b00f4d7b643ddd5415818b02e614e8222
hnagare12 <hnagare12@gmail.com>
2022-04-29 17:33:59 +0530

    employeemgmt

---
+++ BootMVCProj20-MiniProject-Thymleaf/src/main/java/com/nt/BootMvcProj20MiniProjectThymleafApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMvcProj20MiniProjectThymleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMvcProj20MiniProjectThymleafApplication.class, args);
+	}
+
+}
